dddbb8e36be94c8808380bcad3bcfc5d1eba19c3
medmor <elmadioune@gmail.com>
2021-11-13 23:03:04 +0100

    docs

---
+++ SchoolWebsite/docs/wwwroot/SchoolWebsiteBlazor/_content/Havit.Blazor.Components.Web.Bootstrap/HxPopover.js
@@ -0,0 +1,36 @@
+﻿// has to be aligned with HxTooltip.js!
+export function createOrUpdate(element, hxDotnetObjectReference, options) {
+	destroy(element);
+	element.hxDotnetObjectReference = hxDotnetObjectReference;
+	element.addEventListener('shown.bs.popover', handleShown);
+	element.addEventListener('hidden.bs.popover', handleHidden);
+	new bootstrap.Popover(element, options);
+}
+
+export function show(element) {
+	var i = bootstrap.Popover.getInstance(element);
+	i.show();
+}
+
+export function hide(element) {
+	var i = bootstrap.Popover.getInstance(element);
+	i.hide();
+}
+
+function handleShown(event) {
+	event.target.hxDotnetObjectReference.invokeMethodAsync('HxHandleJsShown');
+};
+
+function handleHidden(event) {
+	event.target.hxDotnetObjectReference.invokeMethodAsync('HxHandleJsHidden');
+};
+
+export function destroy(element) {
+	element.removeEventListener('shown.bs.popover', handleShown);
+	element.removeEventListener('hidden.bs.popover', handleHidden);
+	element.hxDotnetObjectReference = null;
+	var popover = bootstrap.Popover.getInstance(element);
+	if (popover) {
+		popover.dispose();
+	}
+}
