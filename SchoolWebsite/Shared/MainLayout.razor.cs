3ac6203b95c1dfe0d085ed77b047cad05104b40a
medmor <elmadioune@gmail.com>
2021-11-13 19:08:19 +0100

    Add project files.

---
+++ SchoolWebsite/Shared/MainLayout.razor.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace SchoolWebsite.Shared
+{
+    public partial class MainLayout
+    {
+        public string dir = "rtl";
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (CultureInfo.CurrentCulture.Name == "ar-AR")
+            {
+                dir = "rtl";
+            }
+            else
+            {
+                dir = "ltr";
+            }
+        }
+    }
+}
