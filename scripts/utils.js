f938affb9eae6b2d4b4b862b208b71cd7155919f
medmor <35731087+medmor@users.noreply.github.com>
2021-11-17 11:36:19 +0000

    Deploying to gh-pages from  @ 99845b49c539283a16915239a97240ed5d37be60 🚀

---
+++ scripts/utils.js
@@ -0,0 +1,18 @@
+﻿
+//*******************************************************************************************************
+//Js run time functions **********************************************************************************
+//*******************************************************************************************************
+
+//culture language config
+window.blazorCulture = {
+    get: () => localStorage['BlazorCulture'],
+    set: (value) => localStorage['BlazorCulture'] = value
+};
+
+//return the view dimensions
+window.getWindowDimensions = function () {
+    return {
+        Width: window.innerWidth,
+        Height: window.innerHeight
+    };
+};
